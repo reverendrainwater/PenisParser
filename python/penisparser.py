42706bd89781200d3223c5153e11659740c39ba3
ZhengC1 <zhengc1@appstate.edu>
2016-02-16 18:18:38 -0500

    Initial Commit

---
+++ python/penisparser.py
@@ -0,0 +1,22 @@
+#!usr/bin/python
+
+def penis():
+    penis = []
+    filename = raw_input("file name?: ")
+
+    text_file =  open(filename, 'r')
+
+    penis = text_file.readlines()
+    print penis
+
+    penis_file = open(name, "a")
+
+    for i in penis: 
+        i.split()
+    name = filename[:-4] + "_with_penis's.txt"
+    print name
+
+
+penis()
+
+
