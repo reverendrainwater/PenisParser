0f31f92e4b693257e32890208a032a343dff23dc
Harcyon <Youngbloodmgregory@gmail.com>
2016-02-25 22:46:42 -0500

    Update penisparser.py

--- python/penisparser.py
+++ python/penisparser.py
@@ -2,12 +2,21 @@
 
 def penis():
     filename = raw_input("file name?: ")
-
+    penis = ""
 
     with open(filename) as f:
         for line in f:
             for word in line.split():
-                print line.replace(str(word), "penis") 
+                if len(word) <= len(penis):
+                    penis = "penis"
+                else:
+                    length = len(word) - 5
+                    penis = "pe"
+                    for penis in length:
+                        penis = penis + "e"
+                    penis = penis + "nis"
+                
+                print line.replace(str(word, penis) 
 penis()
 
 
