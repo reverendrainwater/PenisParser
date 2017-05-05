0ffdf129c6984eb8870208c25722d7ef6095305c
C0urante <cegerton@oberlin.edu>
2017-05-05 07:14:41 -0700

    Add check to see if file is present

--- clojure/penis.clj
+++ clojure/penis.clj
@@ -1,9 +1,14 @@
 (let [filename (first *command-line-args*)]
     (if (nil? filename)
         (println "feed me a file")
-        (let [text (slurp filename)]
-            (spit filename
-                (clojure.string/replace text #"[a-zA-Z0-9]+" "penis")
+        (let [file (clojure.java.io/as-file filename)]
+            (if (not (.exists file))
+                (println "feed me a real file")
+                (let [text (slurp file)]
+                    (spit file
+                        (clojure.string/replace text #"[a-zA-Z0-9]+" "penis")
+                    )
+                )
             )
         )
     )
