8aa47997ccdb1294bb83c8df5e09e61ef8dcf4b8
Unknown <ulfrthewolf@gmail.com>
2017-05-06 11:17:03 +1000

    Added vb.net

---
+++ vb.net/penisparser.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.IO
+
+
+Module Module1
+    Sub Main()
+        Dim path = Console.ReadLine()
+        Dim output = ""
+        Dim input = ""
+        Try
+            Using sr As New StreamReader(path)
+                input = sr.ReadToEnd()
+            End Using
+        Catch e As Exception
+            Console.WriteLine("The file could not be read:")
+            Console.WriteLine(e.Message)
+        End Try
+        Dim tokens As String() = input.Split(" ")
+        For Each word In tokens
+            output += ("penis ")
+        Next
+
+        Console.WriteLine(output)
+        Console.ReadLine()
+    End Sub
+
+End Module
