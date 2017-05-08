8adf120d8ac7fa233276febe2fc80afdd153cd44
Chun Zheng <chun.zheng@bronto.com>
2017-05-07 21:40:39 -0400

    changed php to create output file instead

--- php/ReadWritePenis.php
+++ php/ReadWritePenis.php
@@ -1,5 +1,5 @@
 <?php
-/* Run this file with 'php -f ReadWritePenis.php' */
+/* Run this file with 'php -f readWritePenis.php' */
 echo "Name of your file? : ";
 $prompt = fopen ("php://stdin","r");
 $fileName = trim(fgets($prompt));
@@ -7,6 +7,6 @@ $fileName = trim(fgets($prompt));
 $myFile = file_get_contents($fileName) or die("Unable to open file");
 $pattern = '/\w+/';
 $penifiedText = preg_replace($pattern, 'penis', $myFile);
-file_put_contents($fileName, $penifiedText);
+file_put_contents("output.txt", $penifiedText);
 fclose($prompt);
 ?>
